6171b7e01af29a025cfdbe182929376bdb1fb1f1
David Glivar <davidglivar@gmail.com>
2012-10-11 11:43:52 -0700

    updates

--- jquery.liquidtext.js
+++ jquery.liquidtext.js
@@ -1,53 +1,39 @@
 (function ($) {
-  var methods = {
-    init: function (opts, callback) {
-      return this.each(function () {
-        var original = measure(this);
-        $(this).css({ position: "absolute", height: "auto", width: "auto" });
-        draw(this, original);
-        if (typeof callback === "function") { callback(); }
-      });
-    }
-  };
+    "use strict";
+    var methods = {
+            init: function (options) {
+                return this.each(function () {
+                    var liquidData = measure(this);
+                    draw(this, liquidData);
+                });
+            }
+        };
 
-  function measure(that) {
-    return {
-      w: parseInt($(that).css("width")),
-      fs: parseInt($(that).css("font-size"))
+    function measure(that) {
+        var liquidData = {
+            pw: parseInt($(that).css("width")) - 1,
+            fs: parseInt($(that).css("font-size"))
+        }
+        $(that).css({
+            position: "absolute",
+            width: "auto"
+        });
+        liquidData.nw = that.clientWidth - 1;
+        return liquidData;
+    }
+
+    function draw(that, ld) {
+        var liquidSize = (ld.fs / ld.nw) * ld.pw; 
+        $(that).css({
+            fontSize: Math.round(liquidSize - 2),
+            position: "relative"
+        });
+    }
+
+    $.fn.liquidText = function (method, callback) {
+        methods.init.apply(this, arguments);
+        if (typeof callback === "function") { 
+            callback.call(this);
+        }
     };
-  }
-
-  var draw = function (that, original) {
-    var normalizedW = that.clientWidth,
-        liquidSize  = (original.fs / normalizedW) * original.w,
-        endW;
-
-    $(that).css("font-size", liquidSize);
-    endW = that.clientWidth;
-
-    if (endW < original.w - 5) {
-      original.fs = liquidSize;
-      draw(that, original);
-    } else if (endW > original.w) {
-      original.fs = liquidSize;
-      original.w -= 1;
-      draw(that, original);
-    } else {
-      if ($(that).hasClass("drawn")) { return; }
-      $(that).css("position", "relative");
-      $(that).addClass("drawn");
-      $(that).data("complete", true);
-    }
-  };
-
-  $.fn.liquidText = function (method) {
-    if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-    } else if (typeof method === 'object' || ! method) {
-      return methods.init.apply(this, arguments);
-    } else {
-      $.error('Method ' +  method + ' does not exist on jQuery.liquidText');
-    }    
-  };
-
 }(jQuery));
